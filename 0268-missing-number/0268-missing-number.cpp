f925371131c648ae116f8308f14c945c429753fe
Rishabh Munjal <114298525+Rishabh05022005@users.noreply.github.com>
2024-05-08 13:12:47 +0530

    Time: 11 ms (81.43%), Space: 20.4 MB (31.25%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -6,7 +6,7 @@ public:
 
         //1.Brute Force -> Using Sort STL -- Traversing the array
         
-        sort(nums.begin() , nums.end());
+        /*sort(nums.begin() , nums.end());
 
         for(int i = 0 ; i < nums.size() ; i++){
             if(i != nums[i]){
@@ -14,7 +14,33 @@ public:
             }
         }
 
-        return nums.size();
+        return nums.size();*/
+
+        //2. Optimized (O(1) SPACE & O(N) Runtime)
+
+        //using XOR opertator
+
+        // XOR of two sane no is 0
+
+        int AllXor = 0;
+
+        for(int i = 0 ; i <= nums.size() ; i++){
+            AllXor = AllXor ^ i;
+        }
+
+        for(int num:nums){
+            AllXor = AllXor ^ num;
+        }
+
+        return AllXor;
+
+
+
+
+
+
+
+        
 
 
 
