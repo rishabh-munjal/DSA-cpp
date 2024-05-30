e2e11d950a225b1131e7cfed86f8ba7514554cae
Rishabh Munjal <rishabh5566ind1111@gmail.com>
2024-05-30 23:54:58 +0530

    Matrix

--- Programs/General/Matrix/BoundryTraversal.cpp
+++ Programs/General/Matrix/BoundryTraversal.cpp
@@ -6,7 +6,41 @@ void Boundry(vector <vector<int>> arr){
     int r  = arr.size();
     int c = arr[0].size();
 
-    for(int i = 0)
+
+    if(r==1){
+        for(int i = 0 ; i < c; i++){
+            cout<<arr[0][i]<<" ";
+
+        }
+    }
+
+    else if(c==1){
+        for(int i = 0 ; i < r ; i ++){
+            cout<<arr[i][0]<<" ";
+        }
+    }
+
+    else{
+        
+    for(int i =0 ; i < c ; i++){
+        cout<<arr[0][i]<<" ";
+    }
+
+    for(int i= 1 ; i < r ; i++){
+        cout<<arr[i][c-1]<<" ";
+    }
+
+
+    for(int i = c - 1 ; i >= 0 ; i--){
+        cout<<arr[r-1][i]<<" ";
+    }
+
+
+    for(int i = r - 1 ; i > 0 ; i--){
+        cout<<arr[i][0]<<" ";
+    }
+    }
+
 }
 
 int main()
